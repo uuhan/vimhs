37f3538ffe72c78f50bc8c7e90ca3fc573c4d9a4
uuhan <xuminhui189@gmail.com>
2018-01-08 22:43:51 +0800

    add glsl-completation

--- Main.hs
+++ Main.hs
@@ -4,12 +4,15 @@ import qualified Fibonacci
 import qualified Ghcmod
 import           Neovim
 import qualified Neovim.Ghcid as Ghcid
+import qualified Neovim.Plugins.GLSL as GLSL
 
 main :: IO ()
-main = neovim defaultConfig { plugins = plugins defaultConfig
-                                          <> [ Ghcid.plugin
-                                             , Fibonacci.plugin
-                                             , Ghcmod.plugin
-                                             ]
-                            , logOptions = Just ("/Users/xu/vimhs.log", DEBUG)
-                            }
+main = neovim defaultConfig
+  { plugins = plugins defaultConfig <>
+    [ Ghcid.plugin
+    , Fibonacci.plugin
+    , Ghcmod.plugin
+    , GLSL.plugin
+    ]
+  , logOptions = Just ("/Users/xu/vimhs.log", DEBUG)
+  }
